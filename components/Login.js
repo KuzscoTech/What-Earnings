f42dda3cbea00a1fb3b26ad8ad1636c6a6007a2c
JayPankajPatel <jayp102501@gmail.com>
2021-05-28 20:10:54 -0700

    Login Page UI (no logic implemented yet)

--- components/Login.js
+++ components/Login.js
@@ -61,20 +61,56 @@ import {
 
                 <View style ={styles.passwordUserInputWrapper}>
                     <TextInput
+                        secureTextEntry ={true}
                         style = {styles.passwordTextInput}
                         placeholder = "e.g JohnDoe123"
                     
                     />
                 </View>
                 {/* Forgot Password? */}
-                <TouchableOpacity> 
-
-                    <View style = {styles.forgotpasswordWrapper}>
-                        <Text>Forgot Password?</Text> 
-
+                    <View>
+                        <TouchableOpacity style = {styles.forgotpasswordWrapper}>
+                         <Text style = {styles.forgotpasswordText}>Forgot Password?</Text> 
+                         </TouchableOpacity>
                     </View>
 
-                </TouchableOpacity>
+                
+
+                {/*  Login Container */}
+
+               
+                    <View>
+
+                        <TouchableOpacity style = {styles.loginWrapper}>
+                            <Text style = {styles.loginText}>
+                                Login
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                {/* Don't have an Account? */}
+                <View style = {styles.noaccountWrapper}>
+                    <View style = {styles.noaccountLines} />
+                        <View>
+                            <Text style = {styles.noaccountText}> Don't Have an Account?</Text>
+                        </View>
+                    <View style = {styles.noaccountLines} />
+                </View>
+
+                {/* Sign Up */}
+                <View>
+                    <TouchableOpacity 
+                    style = {styles.signUpWrapper} 
+                    >
+                        <Text style = {styles.signUpText}>Sign up </Text>
+                        
+                    </TouchableOpacity>
+
+                </View>
+                
+
+
+
 
 
 
@@ -157,11 +193,73 @@ const styles = StyleSheet.create({
     },
 
     forgotpasswordWrapper: {
+        marginTop: 10, 
+        marginLeft: 240, 
         
-       
-
-
-
     },
+    forgotpasswordText: {
+        fontFamily: 'Roboto_500Medium', 
+        fontSize: 12, 
+        color: '#A18652'
+
+    }, 
+    loginWrapper: {
+        marginTop: 70, 
+        marginHorizontal:88, 
+        "backgroundColor": "#F5ECDE",
+        "borderWidth": 1,
+        "borderColor": "#CD9F59",
+        "borderStyle": "solid",
+        borderRadius: 50, 
+        paddingVertical: 10,
+        justifyContent: 'center', 
+        alignItems: 'center',
+    },
+    loginText: {
+        fontFamily: 'Roboto_500Medium', 
+        fontSize: 15,
+        letterSpacing: 8, 
+        color: '#A18652', 
+
+    }, 
+    noaccountWrapper: {
+        marginTop:60, 
+        marginHorizontal: 50, 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+    }, 
+    noaccountLines: {
+        flex: 1, 
+        height: 1, 
+        backgroundColor:  '#FDE0B3', 
+        
+    },
+    noaccountText: {
+        fontFamily: 'Roboto_500Medium', 
+        fontSize: 12, 
+        color: '#A18652',
+        paddingHorizontal: 10, 
+    },
+    signUpWrapper: {
+        marginTop: 40, 
+        marginHorizontal:88, 
+        "borderWidth": 1,
+        "borderColor": "#F5ECDE",
+        "borderStyle": "solid",
+        borderRadius: 50, 
+        paddingVertical: 10,
+        justifyContent: 'center', 
+        alignItems: 'center',
+
+    }, 
+    signUpText: {
+        paddingLeft: 20, 
+        fontFamily: 'Roboto_500Medium', 
+        fontSize: 15,
+        letterSpacing: 8, 
+        color: '#FDE0B3', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+    }
 
 });
